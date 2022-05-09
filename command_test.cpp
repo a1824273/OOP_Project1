32295d35abf99afddf57ab4f48d89e5aae680d7b
Lachlan Chalmers <a1839579@adelaide.edu.au>
2022-05-09 13:05:51 +0930

    Command Line Testing

---
+++ command_test.cpp
@@ -0,0 +1,31 @@
+#include <string>
+#include <iostream>
+#include <sstream>
+#include <vector>
+
+using namespace std;
+
+int main()
+{
+  string input;
+  vector<string> command;
+  cout << "Input Command" << endl;
+  getline(cin, input);
+
+  istringstream ss(input);
+  string del;
+
+  while(getline(ss, del, ' '))
+  {
+      command.push_back(del);
+  }
+
+
+  vector<string>::iterator itr = command.begin();
+
+  //cout << command.size();
+
+  for (size_t i = 0; i < command.size(); i++) {
+    cout << command[i] << endl;
+  }
+}
