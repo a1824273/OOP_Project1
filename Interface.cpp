5716ab8b5f2146e1efbf7d8b3902d6e500a2f238
Lachlan Chalmers <a1839579@adelaide.edu.au>
2022-05-09 15:38:41 +0930

    Initial File Setup

---
+++ Interface.cpp
@@ -0,0 +1,3 @@
+#include <Interface.cpp>
+
+Interface::Interface()
---
+++ Interface.h
@@ -0,0 +1,12 @@
+#ifndef INTERFACE_H_
+#define INTERFACE_H_
+
+class Interface
+{
+  private:
+
+  public:
+    Interface();
+};
+
+#endif
