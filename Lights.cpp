3647c9e2e727763c84c625bc902708bd5d569dd9
Tyson Tang <a1824273@student.adelaide.edu.au>
2022-05-12 12:00:37 +0930

    Lights w/o inheritance

---
+++ Lights.cpp
@@ -0,0 +1,54 @@
+#include <iostream>
+#include <string.h>
+#include "Lights.h"
+
+Lights::Lights(){
+    //creating a default light object with a light name, light colour and status of the light
+    thee_light_name = "light1";
+    thee_light_colour = "white";
+    light_on_state = true;
+}
+
+Lights::Lights(std::string light_name, std::string light_colour, bool state_of_light){
+    //making a light variable will the light name, light colour and on off state
+    thee_light_name = light_name;
+    thee_light_colour = light_colour;
+    light_on_state = state_of_light;
+}
+
+void Lights::set_light_name(std::string light_name){
+    //changing the light name
+    thee_light_name = light_name;
+}
+
+std::string Lights::get_light_name(){
+    //getting the light name
+    return thee_light_name;
+}
+
+void Lights::set_state(bool light_state){
+    //change the state of the light (i.e. on or off)
+    light_on_state = light_state;
+
+}
+bool Lights::get_state(){
+    //return the state of the light
+    if (light_on_state == true){
+        std::cout << "The light is on" << std::endl;
+    }
+    else{
+        std::cout << "The light is off" << std::endl;
+    }
+    return light_on_state;
+}
+void Lights::set_colour(std::string colour){
+    //change the colour of the light
+    thee_light_colour = colour;
+}
+std::string Lights::get_colour(){
+    //get the colour of the light
+    return thee_light_colour;
+}
+Lights::~Lights(){
+    //destructor
+}
--- Lights.h
+++ Lights.h
@@ -1,8 +1,41 @@
-#ifdef LIGHTS_H
+#ifndef LIGHTS_H
 #define LIGHTS_H
 
+#include <iostream>
+#include <string>
+
 class Lights{
+private:
+    std::string thee_light_name;
+    std::string thee_light_colour;
+    bool light_on_state;
+public:
+    //creating a default light object with a light name, light colour and status of the light
+    Lights();
+
+    //making a light variable will the light name, light colour and on off state
+    Lights(std::string light_name, std::string light_colour, bool state_of_light);
+
+    //changing the light name
+    void set_light_name(std::string light_name);
+
+    //getting the light name
+    std::string get_light_name();
+
+    //change the state of the light (i.e. on or off)
+    void set_state(bool light_state);
+
+    //return the state of the light
+    bool get_state();
+
+    //change the colour of the light
+    void set_colour(std::string colour);
+
+    //get the colour of the light
+    std::string get_colour();
     
+    //destructor
+    ~Lights();
 };
 
 #endif
