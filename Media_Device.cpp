0b2ac884b2fbbbbb3f372c28c18c7946f1be00f8
Tyson Tang <a1824273@student.adelaide.edu.au>
2022-05-16 14:03:11 +0930

    Media devices removed, test output added

--- Media_Device.cpp
+++ Media_Device.cpp
@@ -4,39 +4,45 @@
 
 Media_Device::Media_Device(){
     //default media device constructor
-    the_media_name = "No media";
     the_media_state = false;
     the_media_volume_level = 0;
     current_channel = "ABC3";
 }
 
-Media_Device::Media_Device(std::string media_name, bool media_state, int media_volume_level, std::string new_channel){
+Media_Device::Media_Device(bool media_state, int media_volume_level, std::string new_channel){
     //media device with user input
-    the_media_name = media_name;
+    //media state level
     the_media_state = media_state;
-    if (media_volume_level >= 0 && media_volume_level <= 100){
+    if (the_media_state == true){
+        std::cout << "The media device is on" << std::endl;
+    }
+    else if(the_media_state == true){
+        std::cout << "The media device is off" << std::endl;
+    }
+    //media volume level
+    if ((media_volume_level >= 0 && media_volume_level <= 100) || (the_media_state == true)){
         the_media_volume_level = media_volume_level;
     }
     else{
         the_media_volume_level = 0;
     }
-    current_channel = new_channel;
-    
-}
-
-void Media_Device::set_media_name(std::string media_name){
-    //setting media name
-    the_media_name = media_name;
-}
-
-std::string Media_Device::get_media_name(){
-    //get media name
-    return the_media_name;
+    std::cout << "The device's volume is: " << the_media_volume_level << std::endl;
+    //media channel
+    if (the_media_state == true){
+        current_channel = new_channel;
+    }
+    std::cout << "You are on " << current_channel << std::endl;
 }
 
 void Media_Device::set_media_state(bool media_state){
     //settin media state
     the_media_state = media_state;
+    if (media_state == true){
+        std::cout << "The media device is on!" << std::endl;
+    }
+    else if(media_state == false){
+        std::cout << "The media device is off!" << std::endl;
+    }
 }
 
 bool Media_Device::get_media_state(){
@@ -54,6 +60,7 @@ void Media_Device::set_media_volume(int media_volume_level){
     else{
         the_media_volume_level = 0;
     }
+    std::cout << "The device's volume is: " << the_media_volume_level << std::endl;
 }
 
 int Media_Device::get_media_volume(){
@@ -62,7 +69,10 @@ int Media_Device::get_media_volume(){
 }
 
 void Media_Device::set_current_channel(std::string new_channel){
-    current_channel = new_channel;
+    if (the_media_state == true){
+        current_channel = new_channel;
+    }
+    std::cout << "You are on " << current_channel << std::endl;
 }
 
 std::string Media_Device::get_current_channel(){
