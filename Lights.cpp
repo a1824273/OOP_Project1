6789326113902a6d927466fb3bb47cb79c69e3cc
Tyson Tang <a1824273@student.adelaide.edu.au>
2022-05-16 13:05:31 +0930

    Lights removed get/set interactable name

--- Lights.cpp
+++ Lights.cpp
@@ -2,28 +2,18 @@
 #include <string.h>
 #include "Lights.h"
 
+using namespace std;
+
 Lights::Lights(){
     //creating a default light object with a light name, light colour and status of the light
-    thee_light_name = interactable_name;
     thee_light_colour = "white";
     light_on_state = true;
 }
 
-Lights::Lights(string light_name, string light_colour, bool state_of_light){
+Lights::Lights(string light_colour, bool state_of_light){
     //making a light variable will the light name, light colour and on off state
-    thee_light_name = light_name;
     thee_light_colour = light_colour;
     light_on_state = state_of_light;
-}
-
-void Lights::set_light_name(string light_name){
-    //changing the light name
-    thee_light_name = light_name;
-}
-
-std::string Lights::get_light_name(){
-    //getting the light name
-    return thee_light_name;
 }
 
 void Lights::set_state(bool light_state){
--- Lights.h
+++ Lights.h
@@ -4,23 +4,18 @@
 #include <iostream>
 #include <string>
 
-class Lights{
+using namespace std;
+
+class Lights: public Interactable{
 private:
-    std::string thee_light_name;
-    std::string thee_light_colour;
+    string thee_light_colour;
     bool light_on_state;
 public:
     //creating a default light object with a light name, light colour and status of the light
     Lights();
 
     //making a light variable will the light name, light colour and on off state
-    Lights(std::string light_name, std::string light_colour, bool state_of_light);
-
-    //changing the light name
-    void set_light_name(std::string light_name);
-
-    //getting the light name
-    std::string get_light_name();
+    Lights(string light_colour, bool state_of_light);
 
     //change the state of the light (i.e. on or off)
     void set_state(bool light_state);
@@ -29,10 +24,10 @@ public:
     bool get_state();
 
     //change the colour of the light
-    void set_colour(std::string colour);
+    void set_colour(string colour);
 
     //get the colour of the light
-    std::string get_colour();
+    string get_colour();
     
     //destructor
     ~Lights();
