0ffd4e4bd2c9f8addc561a59d0d173e710f34802
Abdullah Tahir <m.abbytahir@gmail.com>
2022-05-16 12:37:27 +0930

    Updated Home and Room Files

--- Home.cpp
+++ Home.cpp
@@ -1,6 +1,47 @@
 
 #include <iostream>
 #include <vector>
+#include "Home.h"
+
+Home::Home(){
+    home_name = "N/A"
+    numRooms = 0;
+    rooms = new std::vector<Room>;
+    rooms.clear();
+};
+
+Home::Home(std::string new_name){
+    home_name = new_name;
+    numRooms = 0;
+    rooms = new std::vector<Room>;
+    rooms.clear();
+
+};
+
+void Home::add_room(Room* roomtoAdd){
+    rooms->push_back(roomtoAdd);
+};
+
+void Home::remove_room(Room* roomtoRemove){
+    
+    int position;
+    for (int i = 0; i < rooms.size(); i++){
+        if (rooms[i] = roomtoRemove){
+            position = i;
+        }
+    };
+
+    rooms.erase(position);
+
+};
+
+vector<Room> Home::list_rooms(){
+    return rooms;
+}
+
+Home::~Home(){
+    delete [] rooms;
+}
 
 
 
--- Home.h
+++ Home.h
@@ -11,23 +11,26 @@ class Home{
     protected:
         std::string home_name;
         int numRooms;
-        std::vector<Room> *list; // = new std::vector<Room>;
+        std::vector<Room> *rooms; // = new std::vector<Room>;
 
     public:
         //Default Constructor
-        Home::Home();
+        Home();
 
         //Variable Constructor
-        Home::Home(std::string home_name);
+        Home(std::string new_name);
 
         //add_room Function will add a room to the house
-        void Home::add_room(Room* roomtoAdd);
+        void add_room(Room* roomtoAdd);
 
         //remove_room Function will remove a room from the house
-        void Home::remove_room(Room* roomtoRemove);
+        void remove_room(Room* roomtoRemove);
 
         //list_rooms Function will return the list of rooms in the house
-        vector<Room> Home::list_rooms();
+        vector<Room> list_rooms();
+
+        //Default Destructor
+        ~Home();
 
 };
 
