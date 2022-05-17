4b08743b3ddabfd66a84a50e5033038c3dd3679b
Tyson Tang <a1824273@student.adelaide.edu.au>
2022-05-17 14:53:42 +0930

    .\Lights.cpp with type message

--- Lights.cpp
+++ Lights.cpp
@@ -4,10 +4,11 @@
 #include "Interactable.h"
 
 Lights::Lights(){
-    type = "Lights";
     //creating a default light object with a light name, light colour and status of the light
+    type = "Lights";                //states the type of interactable object
     the_light_colour = "white";     //the colour of the light
     light_on_state = true;          //shows whether or not the light is on (on is true)
+    std::cout << "This object is a " << type << std::endl;
     std::cout << "The light's colour is: " << the_light_colour << "." << std::endl;
     if (light_on_state == true){
         std::cout << "The light is on." << std::endl;
@@ -22,6 +23,7 @@ Lights::Lights(std::string light_colour, bool state_of_light){
     //making a light variable will the light name, light colour and on off state
     the_light_colour = light_colour;
     light_on_state = state_of_light;
+    std::cout << "This object is a " << type << std::endl;
     std::cout << "The light's colour is: " << the_light_colour << "." << std::endl;
     if (light_on_state == true){
         std::cout << "The light is on." << std::endl;
@@ -60,6 +62,7 @@ std::string Lights::get_colour(){
 
 void Lights::print(){
     //printing all current infomation of the light
+    std::cout << "This object is a " << type << std::endl;
     std::cout << "The lights colour is: " << the_light_colour << "." << std::endl;
     if (light_on_state == true){
         std::cout << "The light is on." << std::endl;
