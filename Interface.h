c408dbf7fde7faa5532821a284d263bebbeae2a7
Lachlan Chalmers <lachchalm18@gmail.com>
2022-05-17 14:32:27 +0930

    Syntax Guide Written

--- Interface.h
+++ Interface.h
@@ -7,6 +7,7 @@
 #include <sstream>
 #include <any>
 #include <utility>
+#include <fstream>
 
 
 #include "Room.h"
@@ -25,17 +26,6 @@ class Interface
 {
   private:
       Home * home;
-      /*string bannedNames =
-      {
-          "house",
-          "room",
-          "light",
-          "door",
-          "tv",
-          "speaker",
-          "ac"
-
-      };*/
 
   public:
       //COnstructor takes in a pointer to the home with whcih we will operate on
@@ -47,16 +37,29 @@ class Interface
       //Initiates the console loop
       void console();
 
+      //reads in the input from the user and changes it into a vector of strings
       vector<string> getCommand();
 
+      //figures out what kind of command it is and runs the corresponding function
       bool runCommand(vector<string> command);
 
+      //prints the synta guide from a file
       void helpScreen();
+
+      //is used for the "set" command, changing the values of the objects
       int set(vector<string> command);
+
+      //adding rooms to the house opr interactables to the room
+      //validates input and figures out whether to add room or interactable
       int add(vector<string> command);
+
+      //for removing rooms from the home or interactables from the rooms
       void remove(vector<string> command);
 
+      //returns a pointer the room with name roomName
       Room * findRoom(string roomName);
+
+      //returns pointer to interactable with name deviceName in the room roomToLookIn
       Interactable * findInteractable(string deviceName, Room * roomToLookIn);
       int list(vector<string> command);
 
